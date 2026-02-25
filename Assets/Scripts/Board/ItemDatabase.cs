0f6035d9ea9bbb834734de3ab978a0939d2b84f9
taxuanthang <128502415+taxuanthang@users.noreply.github.com>
2026-02-25 15:57:38 +0700

    Create Database(ScriptableObject) for skin

---
+++ Assets/Scripts/Board/ItemDatabase.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemSkinDatabase", menuName = "Match3/Item Skin Database")]
+public class ItemDatabase : ScriptableObject
+{
+    public Sprite[] normalItemSprites; // size = 7
+}
